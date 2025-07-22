a1932f4f2bb48ec487abfc14f3f9a42b32aa215f
André Veiga <a.branco.veiga@gmail.com>
2025-07-23 07:57:11 +0900

    firs commit

---
+++ lua/plugins/spec1.lua
@@ -0,0 +1,42 @@
+return {
+  -- the colorscheme should be available when starting Neovim
+  {
+    "arcticicestudio/nord-vim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme nord]])
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('telescope.builtin')
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('telescope.builtin')
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  }
+}
